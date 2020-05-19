5bd9b232e38799969e79e4c503608389da6c713d
TaimoorSalyhal <asfandtaimoor786@gmail.com>
2020-05-19 13:42:35 +0500

    Added Next functionality

--- script.js
+++ script.js
@@ -1,27 +1,76 @@
-let xhr = new XMLHttpRequest();
+document.getElementById('nextQ1').addEventListener('click', loadQuestion);
+document.getElementById('nextQ').addEventListener('click', loadQuestion);
 
-xhr.open('GET', 'data.json', true);
+let i = -1;
 
-xhr.onload = function() {
-    if (this.status == 200) {
 
-        let user = JSON.parse(this.responseText);
+// Changing the question after clicking next button
 
-        let output = '';
-        output = `
+function loadQuestion() {
+    // Hiding the start button after click
+    document.getElementById('nextQ1').style.display = 'none';
+
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.json', true);
+
+    xhr.onload = function() {
+        if (this.status == 200) {
+
+            let user = JSON.parse(this.responseText);
+
+            let output = '';
+            output = `
         
         <ul>
-        <li>${user[0].question}</li>
-        <li>${user[0].A}</li>
-        <li>${user[0].B}</li>
-        <li>${user[0].C}</li>
-        <li>${user[0 ].D}</li>
+        <li>${user[i].question}</li>
+        <li onclick='a()'>${user[i].A}</li>
+        <li onclick='b()'>${user[i].B}</li>
+        <li onclick='c()'>${user[i].C}</li>
+        <li onclick='d()'>${user[i].D}</li>
         </ul>`;
 
 
 
-        document.getElementById('userdata').innerHTML = output;
-    }
-}
+            document.getElementById('userdata').innerHTML = output;
 
-xhr.send();
+        }
+    }
+
+    xhr.send();
+
+
+    // 1st option
+    function a() {
+        let Selected = A;
+        console.log('You Selected option A');
+
+    }
+
+    // 2nd option
+
+    function b() {
+        let Selected = B;
+        if (this.user[i].answer == Selected) {
+
+
+            console.log('You Selected option B');
+
+        }
+
+    }
+
+    //3rd option
+    function c() {
+        let Selected = C;
+        console.log('You Selected option C');
+    }
+
+    //4rth option
+    function d() {
+        let Selected = D;
+        console.log('You Selected option D');
+    }
+
+    i++;
+}
